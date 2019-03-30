34d0f5c6d2ef51d46e16f0f27a217f0fe4d2832e
pavitraponns27 <47533247+pavitraponns27@users.noreply.github.com>
2019-03-30 15:25:25 +0530

    Even.c

---
+++ Guvi/Begin/Set1/Q1/Even.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int ch;
+    int flag=0;
+    scanf("%d",&ch);
+    if(ch%2==0)
+    {
+        printf("even");
+        
+    }
+    else
+    {
+        printf("odd");
+        
+    }
+ }
+    
+
+
+ 
+
