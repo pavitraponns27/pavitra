c14bf018e21128cd7d8df33787fae06ae711e52f
pavitraponns27 <47533247+pavitraponns27@users.noreply.github.com>
2019-03-30 21:22:12 +0530

    vowel.py
    Vowel or consonant

--- Guvi/Begin/Set1/Q3/vowel.py
+++ Guvi/Begin/Set1/Q3/vowel.py
@@ -1,1 +1,10 @@
+ch = input(" ");
+if(ch.isalpha()):
+    if(ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I'or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
+    	    print("vowel");
+    else:
+    	print("consonant");
+    
 
+else:
+    print("invalid");
