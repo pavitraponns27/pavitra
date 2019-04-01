395c8595fd7c1ce88e478e21e6ac8331de46ee32
pavitraponns27 <47533247+pavitraponns27@users.noreply.github.com>
2019-04-01 20:18:46 +0530

    primerange.py
    prime number in a range

---
+++ gui/begin/set2/q16/primerange.py
@@ -0,0 +1,9 @@
+m=int(input("enter the lower value"))
+n=int(input("enter the upper value"))
+for num in range(m,n+1):
+      if(num>0):
+            for i in range(2,num):
+                  if(num%i==0):
+                        break;
+            else:
+                  print(num);
