24731b24e0112e7ffbd5a66165b41e1879b29281
pavitraponns27 <47533247+pavitraponns27@users.noreply.github.com>
2019-04-03 19:44:23 +0530

    Update evenrange.py

--- guvi/begin/set2/q15/evenrange.py
+++ guvi/begin/set2/q15/evenrange.py
@@ -1,8 +1,8 @@
-m=int(input("enter the lower value"));
-n=int(input("enter the upper value"));
+m=int(input("enter the lower value"))
+n=int(input("enter the upper value"))
 for i in  range(m,n):
       if(i%2==0):
-         print(i);
+         print(i)
    
       
       
