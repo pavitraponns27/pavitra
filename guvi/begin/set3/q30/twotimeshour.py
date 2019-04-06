e030fac14f9f6a69f5d7a466a7f5c5316d6933f0
pavitraponns27 <47533247+pavitraponns27@users.noreply.github.com>
2019-04-06 14:50:19 +0530

    twotimeshour.py

---
+++ guvi/begin/set3/q30/twotimeshour.py
@@ -0,0 +1,8 @@
+min1=int(input("enter the minute1"))
+min2=int(input("enter the minute2"))
+hr1=int(input("enter hr1"))
+hr2=int(input("enter hr2"))
+hr=hr1-hr2
+min=min1-min2
+print("%d %d"%(hr,min))
+        
