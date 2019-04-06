56b7e5fb5351f16b3e3474f17ed6ca5ec1bf5836
pavitraponns27 <47533247+pavitraponns27@users.noreply.github.com>
2019-04-06 18:42:26 +0530

    digitsinstring.c
    number of digits in a string

---
+++ guvi/begin/set4/q35/digitsinstring.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+ 
+void main()
+{
+    char s[200];
+    int digit= 0, i;
+ 
+    printf("enter the string\n");
+    scanf("%[^\n]s", s);
+    for (i = 0;s[i] != '\0';i++)
+ {
+        if ((s[i]>='0'&&s[i]<='9'))
+            digit++;    
+    }
+    printf("%d", digit);
+}
+
+
+
