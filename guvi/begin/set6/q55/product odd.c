cafdbc747a7edebc0553e732e31da61e28d561ff
pavitraponns27 <47533247+pavitraponns27@users.noreply.github.com>
2019-07-31 20:24:29 +0530

    product odd.c
    product of a number is odd or even

---
+++ guvi/begin/set6/q55/product odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int x,y,z;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    z=x*y;
+    if(z%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    
+}
