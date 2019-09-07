7aa0b434768a4a4f6bfa307440b79e9becba7cce
pavitraponns27 <47533247+pavitraponns27@users.noreply.github.com>
2019-09-07 19:22:54 +0530

    oddevenstr.c

---
+++ guvi/basics/oddevenstr.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+char s[20],i;
+scanf("%s",s);
+i=0;
+while(s[i]!='\0')
+{
+printf("%c",s[i]);
+i=i+2;
+}
+printf("\n");
+i=1;
+while(s[i]!='\0')
+{
+printf("%c",s[i]);
+i=i+2;
+}
+}
