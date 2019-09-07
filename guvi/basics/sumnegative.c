79d184a866171c377daf76e2321052162181576a
pavitraponns27 <47533247+pavitraponns27@users.noreply.github.com>
2019-09-07 20:02:43 +0530

    sumnegative.c

---
+++ guvi/basics/sumnegative.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a[20],n,sum=0,i;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]<0)
+{
+sum=sum+a[i];
+}
+}
+printf("%d",sum);
+}
